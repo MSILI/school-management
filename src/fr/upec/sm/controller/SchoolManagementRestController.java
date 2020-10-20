4c0118e2b78ecc33391c4e173ff8f87b5a989b77
fatah <fatahmsili@gmail.com>
2020-10-21 01:00:06 +0200

    solve problem with ressource not found

--- src/fr/upec/sm/controller/SchoolManagementRestController.java
+++ src/fr/upec/sm/controller/SchoolManagementRestController.java
@@ -13,6 +13,9 @@ import fr.upec.sm.model.Assistant;
 import fr.upec.sm.model.Module;
 import fr.upec.sm.model.Professeur;
 
+@Path("/api")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SchoolManagementRestController {
 
 	private DBManagement dBManagement;
@@ -23,45 +26,36 @@ public class SchoolManagementRestController {
 
 	@GET
 	@Path("/professeurs")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response findAllProffesors() {
 		return Response.status(Response.Status.OK).entity(dBManagement.listerProfesseurs()).build();
 	}
 
 	@GET
 	@Path("/assistants")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response findAllAssistants() {
 		return Response.status(Response.Status.OK).entity(dBManagement.listerAssistants()).build();
 	}
 
 	@GET
 	@Path("/modules")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response findAllModules() {
 		return Response.status(Response.Status.OK).entity(dBManagement.listerModules()).build();
 	}
 
 	@POST
 	@Path("/professeurs")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response saveProfesseur(Professeur professeur) {
 		return Response.status(Response.Status.CREATED).entity(dBManagement.ajouterProfesseur(professeur)).build();
 	}
 
 	@POST
 	@Path("/assistants")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
 	public Response saveAssistant(Assistant assistant) {
 		return Response.status(Response.Status.CREATED).entity(dBManagement.ajouterAssistant(assistant)).build();
 	}
 
 	@POST
 	@Path("/modules")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
 	public Response saveModule(Module module) {
 		return Response.status(Response.Status.CREATED).entity(dBManagement.ajouterModule(module)).build();
 	}
